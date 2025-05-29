b8196a00e074c4f796ab4dfc36e2467e7175ccc4
Cormac Mulvenna <cormacmulvenna@Cormacs-MacBook-Air.local>
2025-05-29 17:58:14 +0100

    mobile nav bar and image fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cormacs-portfolio.vercel.app', // PLEASE UPDATE THIS with your actual URL
   integrations: [tailwind()],
 });
